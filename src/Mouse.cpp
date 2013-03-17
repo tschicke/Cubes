4526d686e2874abe22aacb36e0d2a583a5791caf
Tyler Schicke <tschicke@optonline.net [credential]>
2013-03-16 21:51:10 -0400

    Added and implemented Mouse class

---
+++ src/Mouse.cpp
@@ -0,0 +1,51 @@
+/*
+ * Mouse.cpp
+ *
+ *  Created on: Mar 16, 2013
+ *      Author: Tyler
+ */
+
+#include "Mouse.h"
+
+namespace ts {
+
+glm::vec2 Mouse::position;
+bool Mouse::locked;
+int Mouse::lastDX, Mouse::lastDY;
+
+Mouse::Mouse() {
+}
+
+Mouse::~Mouse() {
+}
+
+void Mouse::setPosition(int x, int y){
+	position = glm::vec2(x, y);
+}
+
+glm::vec2 Mouse::getPosition(){
+	return position;
+}
+
+void Mouse::movePosition(int dx, int dy){
+	position += glm::vec2(dx, dy);
+}
+
+void Mouse::setLastMove(int dx, int dy){
+	lastDX = dx;
+	lastDY = dy;
+}
+
+glm::vec2 Mouse::getLastMove(){
+	return glm::vec2(lastDX, lastDY);
+}
+
+void Mouse::setLocked(bool isLocked){
+	locked = isLocked;
+}
+
+bool Mouse::isLocked(){
+	return locked;
+}
+
+} /* namespace ts */
