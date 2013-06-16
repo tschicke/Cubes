9e880d0390ad59866f34defb6ebf74936f84eddd
Tyler Schicke <tschicke@optonline.net>
2013-06-16 09:33:13 -0400

    added HUD class

---
+++ src/HudLayer.cpp
@@ -0,0 +1,26 @@
+/*
+ * HudLayer.cpp
+ *
+ *  Created on: Jun 16, 2013
+ *      Author: Tyler
+ */
+
+#include "HudLayer.h"
+
+HudLayer::HudLayer() {
+	// TODO Auto-generated constructor stub
+
+}
+
+HudLayer::~HudLayer() {
+	// TODO Auto-generated destructor stub
+}
+
+void HudLayer::draw() {
+}
+
+void HudLayer::update(time_t dt) {
+}
+
+void HudLayer::handleInput() {
+}
